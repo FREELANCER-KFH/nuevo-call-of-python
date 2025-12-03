b9687b3a171d2bd81a8658ee50b9552c0a24a318
Kevin Feliz Henriquez <108124473+FREELANCER-KFH@users.noreply.github.com>
2025-12-03 16:04:21 +0000

    agregando el autor con estilos, desde js y css

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const body = document.querySelector('body')
 const klk = document.createElement('h3')
 klk.textContent = 'klk con klk mi gente'
 
+const autor = document.createElement('p')
+autor.textContent = 'Kevin Feliz Henriquez (kfh freelancer)'
+
 body.appendChild(klk)
+body.appendChild(autor)
